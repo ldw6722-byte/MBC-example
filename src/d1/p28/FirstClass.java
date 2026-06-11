1bf5b611b9c9e511642defaf53c1005156f88850
leed <ldw6722@gmail.com>
2026-06-11 16:27:54 +0900

    0611

---
+++ src/d1/p28/FirstClass.java
@@ -0,0 +1,14 @@
+package d1.p28;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+//        System.out.println("이 문장이 실행될까요?...1");
+
+        System.out.println("이 문장이 실행될까요?...2");
+
+//        System.out.println("이 문장이 실행될까요?...3");
+//        System.out.println("이 문장이 실행될까요?...4");
+    }
+}
