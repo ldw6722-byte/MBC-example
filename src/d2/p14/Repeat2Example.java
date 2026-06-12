9d37a2f865a595e4729dc056f3cf61192637a0dc
leed <ldw6722@gmail.com>
2026-06-12 18:27:41 +0900

    0612

---
+++ src/d2/p14/Repeat2Example.java
@@ -0,0 +1,9 @@
+package d2.p14;
+
+public class Repeat2Example {
+    public static void main(String[] args) {
+        for(int i=1;i<=10;i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/d2/p16/ForSumExample.java
@@ -0,0 +1,18 @@
+package d2.p16;
+
+import java.util.Scanner;
+
+public class ForSumExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int inp = sc.nextInt();
+        int sum = 0;
+
+        for(int i=1; i<=inp; i++) {
+            sum += i;
+        }
+
+        System.out.println("sum = " + sum);
+        sc.close();
+    }
+}
